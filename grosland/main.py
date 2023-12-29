80f2cb17a3387567eacd100a623f12b9728fe61c
zemfak <zemfak1930@gmail.com>
2023-12-29 18:01:25 +0200

    add search to cadastre

--- grosland/main.py
+++ grosland/main.py
@@ -1,12 +1,26 @@
+from grosland.models import Cadastre
+
+from flask import render_template, request, jsonify
+
 from flask_security import login_required
 
 from grosland.app import app, session
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
-    return "Все ОК"
+    if request.method == "POST":
+        geojson = session.query(Cadastre.geometry.ST_AsGeoJSON()).filter_by(
+            cadnum=request.form.to_dict()["cadnum"]
+        ).first()
+
+        if geojson:
+            return jsonify({
+                "coordinates": eval(geojson[0])["coordinates"]
+            })
+
+    return render_template("index.html")
 
 
 @app.teardown_appcontext
