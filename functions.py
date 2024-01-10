cd701998ea37bc6d7cfc21d2d5c54558c586e777
zemfak <zemfak1930@gmail.com>
2024-01-10 22:59:39 +0200

    fixed get_parcels

--- functions.py
+++ functions.py
@@ -1,20 +1,29 @@
 from grosland.app import session
-from grosland.models import Cadastre
+from grosland.models import Cadastre, Archive
+import time
 
 
-def get_cadastre(koatuu: str):
+def get_parcels(koatuu: str, layer: str = 'cadastre', write: bool = False):
     """
-        Accepts a KOATUU and returns a list of cadastral objects of the required KOATUU.
-        Additionaly creates a list of cadastre in a given KOATUU.
-        :param koatuu: e.q. 5121680800
+        Accepts a KOATUU and returns a list of cadastre/archive objects of the required KOATUU.
+        Additionaly creates a file with a list of the cadastre/archive in a given KOATUU.
+        :param  koatuu: e.q. '5121680800' --> Search parameter by layer,
+                layer: 'cadastre' or 'archive' --> In which layer to look for data,
+                write: bool --> Whether to write data to file or not.
         :return: [object, object, ...]
     """
+    #   Search model definition
+    model = Cadastre if layer == 'cadastre' else (Archive if layer == 'archive' else None)
+
+    #   Database search
     cadastre_list = (
-        session.query(Cadastre).filter(Cadastre.cadnum.like(f'{koatuu[0:8]}%')).order_by(Cadastre.cadnum.asc()).all()
+        session.query(model).filter(model.cadnum.like(f'{koatuu[0:8]}%')).order_by(model.cadnum.asc()).all()
     )
 
-    with open(f"temp/{koatuu[0:8] + '00'}.txt", "a") as file:
-        for cadastre in cadastre_list:
-            file.write(f'{cadastre.cadnum}\n')
+    #   Writing data to a file. Optional
+    if write:
+        with open(f"temp/{layer}_{koatuu[0:8] + '00'}_{int(time.time())}.txt", "a") as file:
+            for cadastre in cadastre_list:
+                file.write(f'{cadastre.cadnum}\n')
 
     return cadastre_list
