6b36402b70b8ac5fd73e39067e23b13c97dbfa78
zemfak <zemfak1930@gmail.com>
2024-01-23 22:28:46 +0200

    add: function for add layer objects

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+from geoalchemy2 import WKTElement
+
+from geomet import wkt
+
 from grosland.app import session
 from grosland.models import Cadastre, Archive
 
@@ -8,9 +12,9 @@ def get_parcels(koatuu: str, layer: str = 'cadastre', write: bool = False):
     """
         Accepts a KOATUU and returns a list of cadastre/archive objects of the required KOATUU.
         Additionaly creates a file with a list of the cadastre/archive in a given KOATUU.
-        :param  koatuu: e.q. '5121680800' --> Search parameter by layer,
-                layer: 'cadastre' or 'archive' --> In which layer to look for data,
-                write: bool --> Whether to write data to file or not.
+        :params  koatuu: e.q. '5121680800' --> Search parameter by layer,
+                 layer: 'cadastre' or 'archive' --> In which layer to look for data,
+                 write: bool --> Whether to write data to file or not.
         :return: [object, object, ...]
     """
     #   Search model definition
@@ -28,3 +32,41 @@ def get_parcels(koatuu: str, layer: str = 'cadastre', write: bool = False):
                 file.write(f'{cadastre.cadnum}\n')
 
     return cadastre_list
+
+
+def create_db_object(**kwargs):
+    """
+        Accepts a dictionary with parameters required to execute the function.
+        Reads geojson file and adds objects to database.
+        Only for Cadastre or Archive classes.
+    :param kwargs: {
+        "file": str = path to .geojson file,
+        "layer: str = "cadastre" or "archive",
+
+        "cadnum": str = column name in the .geojson file to communicate with the class,
+        "ownership_code": str,
+        "purpose_code": str,
+        "area": str,
+    }
+    :return: None
+    """
+    model = Cadastre if kwargs["layer"] == "cadastre" else (Archive if kwargs["layer"] == "archive" else None)
+
+    if model:
+        geojson = eval(open(kwargs["file"]).read().replace("null", "None"))
+
+        for item in geojson["features"]:
+            new_object = model(
+                cadnum=item["properties"][kwargs["cadnum"]],
+                ownership_code=str(item["properties"][kwargs["ownership_code"]]),
+                purpose_code=str(item["properties"][kwargs["purpose_code"]]),
+                area=item["properties"][kwargs["area"]],
+                address=item["properties"][kwargs["address"]],
+                geometry=WKTElement(wkt.dumps({
+                    "type": item["geometry"]["type"],
+                    "coordinates": item["geometry"]["coordinates"]
+                }), srid=4326)
+            )
+
+            session.add(new_object)
+        session.commit()
