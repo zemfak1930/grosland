3d35dba9ab7cca5b96694644a938380d50ae1a81
zemfak <zemfak1930@gmail.com>
2024-01-29 00:54:11 +0200

    fix: add search parameters for get_parcels_list

--- grosland/blueprints/api.py
+++ grosland/blueprints/api.py
@@ -1,4 +1,4 @@
-from flask import abort, Blueprint, jsonify
+from flask import abort, Blueprint, jsonify, request
 
 from flask_security import login_required
 
@@ -30,10 +30,46 @@ def get_parcels_list():
     }
     """
     parcels = {}
+    base_operator = ["==", ">=", "<=", "!="]
+    arg_area = None
 
     for model in [Cadastre, Archive]:
+        query_filter = []
+
+        for item in ["cadnum", "area", "ownership_code", "purpose_code", "address"]:
+            if request.args.get(item):
+                #   Params "area"   ------------------------------------------------------------------------------------
+                if item == "area":
+                    area = request.args.get(item)
+                    operator = area[:2]
+
+                    try:
+                        arg_area = float(area[2:].replace(",", "."))
+                    except ValueError:
+                        pass
+
+                    if operator in base_operator and arg_area and arg_area >= 0:
+                        for op in base_operator:
+                            if op == operator:
+                                query_filter.append(
+                                    eval(f"model.{item} {operator} {str(arg_area)}")
+                                )
+
+                #   Params "cadnum" / "ownership_code" / "purpose_code" / "address" ------------------------------------
+                else:
+                    query_filter.append(
+                        eval(
+                            f"model.{item}.ilike('" +
+                            ("%" if item in ["address", "cadnum"] else "") +    # If we have address/cadnum parameter
+                            f"{request.args.get(item)}%')"
+                        )
+                    )
+
+        #   Run filter  ------------------------------------------------------------------------------------------------
         parcels.update({
-            model.__name__: [item.cadnum for item in session.query(model).all()]
+            model.__name__: [
+                item.cadnum for item in session.query(model).filter(*query_filter).order_by(model.cadnum.asc()).all()
+            ]
         })
 
     return jsonify(parcels)
